c8abe1cacf45a204661c8f7ca0e5af1b5866a8b6
Eggyf <abdelfregel1711@gmail.com>
2024-08-23 20:40:25 -0400

    tournament logic

--- logic/tournament_logic.py
+++ logic/tournament_logic.py
@@ -1,18 +1,63 @@
+import random
+
+
 class Player:
-    def __init__(self, name) -> None:
+    def __init__(self, name, points=0, subscribed=False) -> None:
         self.name = name
+        self.points = points
+        self.subscribed = subscribed
 
 
 class Tournament:
-    def __init__(self, name, rounds, num_players) -> None:
+    def __init__(self, name, rounds, num_players, started=False) -> None:
         self.name = name
         self.rounds = rounds
         self.num_players = num_players
         self.players = []
+        self.started = started
+
+    def start_tournament(self):
+        self.started = True
+        for player in self.players:
+            player.subscribed = True
+            player.points = 0
 
     def add_player(self, player: Player):
-        if self.players.count() > self.num_players:
+        if (
+            len(self.players) <= self.num_players
+            and player.subscribed == False
+            and not self.started
+        ):
             self.players.append(player)
-            return "Player has been added"
+            print(f"Player {player.name} has been added")
         else:
-            return "Tournament is full"
+            print(
+                "Tournament is full or started or player is already in another tournament"
+            )
+
+    def simulate(self):
+        for r in range(self.rounds):
+            for player in self.players:
+                player.points = random.randint(3, 10)
+        print("tournament simulated")
+
+    def set_winner(self):
+        winner = self.players[0]
+        for player in self.players:
+            if winner.points < player.points:
+                winner = player
+        for player in self.players:
+            player.subscribed = False
+        return winner
+
+
+player1 = Player("Abdel")
+player2 = Player("Dayelin")
+player3 = Player("Arian")
+tournament = Tournament("torneo 1", 10, 3)
+tournament.add_player(player1)
+# tournament.start_tournament()
+tournament.add_player(player2)
+tournament.add_player(player3)
+tournament.simulate()
+print(f"The winner in the {tournament.name} is {tournament.set_winner().name}")
